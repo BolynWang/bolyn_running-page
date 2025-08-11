21cd3dadaa2f9cd770b4b7928053469810410160
JiazanWang <1710998763@qq.com>
2025-08-11 19:14:01 +0800

    第一次初始化

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,38 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const getBasePath = () => {
+  const baseUrl = import.meta.env.BASE_URL;
+  return baseUrl === '/' ? '' : baseUrl;
+};
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Running Page',
+  siteUrl: 'https://run.wbolyn.com',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: '王博霖的个人运动记录',
+  navLinks: [
+    {
+      name: 'Summary',
+      url: `${getBasePath()}/summary`,
+    },
+    {
+      name: 'Blog',
+      url: 'https://wbolyn.com',
+    },
+    {
+      name: 'About',
+      url: 'https://wbolyn.com/about',
+    },
+  ],
+};
+
+export default data;
